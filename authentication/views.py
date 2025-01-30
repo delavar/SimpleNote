3c016950719685b04b8a004a5c82560ee6dbbb55
Alireza Delavar <alireza.delavar@divar.ir>
2025-01-30 13:51:05 +0330

    add user info api

--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,22 @@
 from django.contrib.auth.models import User
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
-from .serializers import RegisterSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .serializers import RegisterSerializer, UserInfoSerializer
 
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
+
+class UserInfoView(APIView):
+    serializer_class = UserInfoSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserInfoSerializer(request.user)
+        return Response(serializer.data)
