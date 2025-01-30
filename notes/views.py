c8cbe29436d3d2988fdaababc76f59a0fbd2159f
Alireza Delavar <alireza.delavar@divar.ir>
2025-01-30 17:43:39 +0330

    add batch create

--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,10 @@
-from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView, CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from django_filters import rest_framework as filters
+from rest_framework.response import Response
+
 from .models import Note
 from .permissions import IsOwnerOrReadOnly
 from .serializers import NoteSerializer
@@ -13,6 +17,26 @@ class ListCreateNoteAPIView(ListCreateAPIView):
     queryset = Note.objects.all()
     permission_classes = [IsAuthenticated]
     pagination_class = CustomPagination
+
+    def create(self, request, *args, **kwargs):
+        # Override create to support batch creation
+        if isinstance(request.data, list):
+            # When request data is a list, handle batch creation
+            serializer = self.get_serializer(data=request.data, many=True)
+            if serializer.is_valid():
+                serializer.save(creator=self.request.user)
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                # Detail validation error for each item
+                errors = []
+                for index, error in enumerate(serializer.errors):
+                    if error:
+                        error['index'] = index
+                        errors.append(error)
+                return Response(errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            # Default single instance creation
+            return super().create(request, *args, **kwargs)
 
     def get_queryset(self):
         return self.queryset.filter(creator=self.request.user)
@@ -41,4 +65,3 @@ class FilterNotesAPIView(ListCreateAPIView):
     def perform_create(self, serializer):
         # Assign the user who created the note
         serializer.save(creator=self.request.user)
-
